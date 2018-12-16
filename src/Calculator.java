b95108e26247f8cf5cb05c86be8300d7f97556ce
Zied Naimi <zied.naimi@heig-vd.ch>
2018-12-16 16:10:51 +0100

    calculator mode console

--- src/Calculator.java
+++ src/Calculator.java
@@ -4,58 +4,71 @@ import java.util.Map;
 import java.util.Scanner;
 
 public class Calculator {
+	private State state;
 
-    private State state;
+	public Calculator(State state) {
+		this.state = state;
+	}
 
+	private void ajoutDeFonctionalite(Map<String, Operator> operatorMap, State state) {
+		// BinaryOperation
+		operatorMap.put("+", new Add(state));
+		operatorMap.put("-", new Substract(state));
+		operatorMap.put("*", new Multiply(state));
+		operatorMap.put("/", new Divide(state));
 
+		// UnaryOperation
+		operatorMap.put("sqrt", new SquareRoot(state));
+		operatorMap.put("x^2", new Square(state));
+		operatorMap.put("1/x", new Inverse(state));
+		operatorMap.put("+/-", new MoreLess(state));
 
-    public Calculator(State state) {
-        this.state = state;
+		// MemoryOperation
+		operatorMap.put("MR", new MR(state));
+		operatorMap.put("MS", new MS(state));
+		operatorMap.put("<=", new Backspace(state));
+		operatorMap.put("CE", new CE(state));
+		operatorMap.put("C", new C(state));
 
+		// TODO Assistant: rajoute ta methode ici connard!
 
-    }
+	}
 
-    public void calcul() {
+	public void calcul() {
 
-        State state = new State();
-        Map<String, Operator> operatorMap = new HashMap<>();
-        init(operatorMap, state);
+		State state = new State();
+		Map<String, Operator> operatorMap = new HashMap<>();
+		ajoutDeFonctionalite(operatorMap, state);
 
-        Scanner sin = new Scanner(System.in);
-        boolean isFirstEntry = true;
-        while (sin.hasNext()) {
+		Scanner sin = new Scanner(System.in);
+		boolean isFirstEntry = true;
 
-            System.out.print("> ");
+		System.out.print("> ");
+		while (sin.hasNext()) {
+			double d = 0;
+			if (sin.hasNextDouble()) { // l'entree est un réel
+				if (!isFirstEntry && !state.isEvaluated())
+					new Enter(state).execute();
 
-            if (sin.hasNextDouble()) {
-                if(!isFirstEntry)new Enter(state).execute();
-                double d = sin.nextDouble();
-                new Number(Double.toString(d),state).execute();
+				d = sin.nextDouble();
+				new Number(Double.toString(d), state).execute();
 
+			} else {
+				String entry = sin.next();
+				if (operatorMap.containsKey(entry)) {
+					operatorMap.get(entry).execute();
+				}
+			}
+				System.out.println("Current Value : " + state.getInput() + " Stack : " + Arrays.toString(state.getStack()));
+			isFirstEntry = false;
+			System.out.print("> ");
+		}
 
+	}
 
-            } else {
+	public static void main(String[] args) {
+		Calculator calc = new Calculator(new State());
+		calc.calcul();
+	}
 
-                String entry = sin.next();
-                if (operatorMap.containsKey(entry)){
-                    operatorMap.get(entry).execute();
-                }
-            }
-            System.out.println("Current Value : " + state.getCurrentVal() + " Stack : " + Arrays.toString(state.getStack()) + " Memory : " + state.getMemoryStore());
-            isFirstEntry= false;
-        }
-
-    }
-
-    public static void main(String[] args) {
-        Calculator calc = new Calculator(new State());
-        calc.calcul();
-    }
-
-    private static void init(Map<String, Operator> operatorMap, State state) {
-        operatorMap.put("+", new Add(state));
-        operatorMap.put("-", new Substract(state));
-        operatorMap.put("*", new Multiply(state));
-        operatorMap.put("/", new Divide(state));
-    }
 }
