568abe57e4c4baa3e7d82f14f2a31694eea71c06
guitch94 <guillaume.blanco@heig-vd.ch>
2018-12-17 09:58:50 +0100

    Commentaire Exit

--- src/ExitOperator.java
+++ src/ExitOperator.java
@@ -3,10 +3,14 @@
  * @author Zouari Adam, Blanco Guillaume & Zied Naimi
  * @version 1.0
  *
- *
+ * La classe ExitOperator nous permet, en mode console, de sortir de notre application
  *
  */
 public class ExitOperator extends Operator {
+
+	/**
+	 * la méthode execute redéfinie nous permet de sortir de notre application
+	 */
 	@Override
 	public void execute() {
 		System.exit(0);
